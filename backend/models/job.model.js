3cf6777a5d27fe0575b06221ca6d8a321bbec977
22UCS097 <ravikumar628763@gmail.com>
2025-07-03 13:03:55 +0530

    job model added

---
+++ backend/models/job.model.js
@@ -0,0 +1,54 @@
+import mongoose, { mongo } from "mongoose";
+
+const jobSchema=new mongoose.Schema({
+     title:{
+         type:String,
+         required:String,
+     },
+      description:{
+         type:String,
+         required:String,
+     },
+      requiredments:{
+         type:String,
+         required:String,
+     },
+      salary:{
+         type:String,
+         required:String,
+     },
+     location:{
+        type:String,
+         required:true
+     },
+     jobType:{
+        type:String,
+        required:true
+     },
+     position:{
+        type:Number,
+        required:true,
+     },
+     company:{
+         type:mongoose.Schema.Types.ObjectId,
+         ref:'Company',
+         required:true,
+     },
+     created_by:{
+        type:mongoose.Schema.Types.ObjectId,
+         ref:'User',
+         required:true,
+     },
+     applications:[
+        {
+          type:mongoose.Schema.Types.ObjectId,
+         ref:'Application',
+           
+        }
+     ]
+
+
+});
+
+export const  Job=mongoose.model("Job",jobSchema);
+
